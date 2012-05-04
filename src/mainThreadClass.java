b7c7615f8c706f590657befe21fda1d5005cf77b
huangyingw@linux_programming <huangyingw@gmail.com>
2012-05-03 21:51:53 -0500

    n

--- src/mainThreadClass.java
+++ src/mainThreadClass.java
@@ -1,10 +1,14 @@
 public class mainThreadClass {
 	public static void main(String[] args) {
-		System.out.println("Inside main  ");
-		int i;
-		for (i = 1; i <= 10; i++) {
-			PrimeThread p = new PrimeThread(1, 200000);
-			p.start();
-		}
+		PrimeThread p1 = new PrimeThread(1, 200000);
+		PrimeThread p2 = new PrimeThread(200001, 400000);
+		PrimeThread p3 = new PrimeThread(400001, 600000);
+		PrimeThread p4 = new PrimeThread(600001, 800000);
+		PrimeThread p5 = new PrimeThread(800001, 1000000);
+		p1.start();
+		p2.start();
+		p3.start();
+		p4.start();
+		p5.start();
 	}
 }
